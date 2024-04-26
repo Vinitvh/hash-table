b135926829e210a193724a98ee4d08b46ab1f1f4
Vinitvh <vineet.vh@gmail.com>
2024-04-26 21:19:53 +0530

    Implement print and insert

--- main.c
+++ main.c
@@ -15,6 +15,14 @@ typedef struct HashTable {
   HT_Item **items;
 } HashTable;
 
+unsigned int hash(char *key) {
+  unsigned int hash = 0;
+  for (unsigned int i = 0; i < strlen(key); i++) {
+    hash += (i * key[i]) / 31;
+  }
+  return hash % HASHTABLE_SIZE;
+}
+
 HT_Item *create_item(char *key, char *val) {
   HT_Item *item = malloc(sizeof(HT_Item));
   item->key = malloc(strlen(key) + 1);
@@ -25,8 +33,8 @@ HT_Item *create_item(char *key, char *val) {
 }
 
 HashTable *create_table() {
-  HashTable *table = malloc(sizeof(HashTable));
-  table->items = calloc(HASHTABLE_SIZE, sizeof(HT_Item));
+  HashTable *table = (HashTable *)malloc(sizeof(HashTable));
+  table->items = (HT_Item *)calloc(HASHTABLE_SIZE, sizeof(HT_Item));
   for (int i = 0; i < HASHTABLE_SIZE; i++) {
     table->items[i] = NULL;
   }
@@ -53,8 +61,34 @@ void free_table(HashTable *table) {
   free(table);
 }
 
+HashTable *insert(HashTable *table, char *key, char *val) {
+  unsigned int index = hash(key);
+  for (int i = 0; i < HASHTABLE_SIZE; i++) {
+    // if (table->items != NULL) {
+    //   printf("Space occupied");
+    //   exit(1);
+    // }
+    strncpy(table->items[index]->val, val, strlen(val));
+  }
+  return table;
+}
+
+void print_table(HashTable *table) {
+  if (table == NULL) {
+    printf("No hashtable to print");
+  }
+  for (int i = 0; i < HASHTABLE_SIZE; i++) {
+    if (table->items[i]) {
+      printf("%s\n", table->items[i]->val);
+    } else {
+      printf("%d. _____\n", i);
+    }
+  }
+}
+
 int main(void) {
-  HT_Item *res = create_item("Hello", "10");
-  printf("%s\n", res->val);
+  HashTable *table = create_table();
+  insert(table, "one", "10");
+  print_table(table);
   return 0;
 }
